a031b0ee149ab72af08a90d3e42a96b64f295083
Ryan Bigg <me@ryanbigg.com>
2020-01-15 15:48:08 +1100

    Details done

--- src/Cafe/__tests__/Details.js
+++ src/Cafe/__tests__/Details.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Details from "../Details";
 
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 const name = "Kinfolk";
 
@@ -13,10 +13,22 @@ test("shows the cafe's name", () => {
 
 test("toggle hours", () => {
   const openingHours = ["CLOSED", "7-2", "7-2", "7-2", "7-2", "7-2", "CLOSED"];
+  const today = new Date();
 
   const { getByText, queryByText } = render(
     <Details dayOfTheWeek={1} name={name} openingHours={openingHours} />
   );
+
+  expect(getByText("Today (Monday): 7-2")).toBeInTheDocument();
+  expect(queryByText("Tuesday: 7-2")).toBeNull();
+
+  fireEvent.click(getByText("Show All Hours"));
+
+  expect(getByText("Tuesday: 7-2")).toBeInTheDocument();
+  expect(getByText("Wednesday: 7-2")).toBeInTheDocument();
+  expect(getByText("Thursday: 7-2")).toBeInTheDocument();
+
+  fireEvent.click(getByText("Show Today's Hours"));
 
   expect(getByText("Today (Monday): 7-2")).toBeInTheDocument();
   expect(queryByText("Tuesday: 7-2")).toBeNull();
