5925b7e86e8d06e1d6819e4eda1cf7e6081355e1
Ryan Bigg <me@ryanbigg.com>
2020-01-15 12:03:38 +1100

    Split opening hours into its own component

--- src/Cafe/Details.js
+++ src/Cafe/Details.js
@@ -1,67 +1,6 @@
 import React from "react";
 import axios from "axios";
-
-class OpeningHours extends React.Component {
-  state = {
-    showAll: false
-  };
-
-  days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-
-  toggleShowAll = () => {
-    this.setState({ showAll: !this.state.showAll });
-  };
-
-  renderAllHours() {
-    return (
-      <div>
-        <ul>
-          {this.props.hours.map((hours, i) => (
-            <li key={i}>
-              {this.days[i]}: {hours}
-            </li>
-          ))}
-        </ul>
-        <a onClick={this.toggleShowAll} href="#">
-          Show Today's Hours
-        </a>
-      </div>
-    );
-  }
-
-  renderTodaysHours() {
-    const { hours, dayOfTheWeek } = this.props;
-    return (
-      <div>
-        <ul>
-          <li>
-            Today ({this.days[dayOfTheWeek]}): {hours[dayOfTheWeek]}
-          </li>
-        </ul>
-        <a onClick={this.toggleShowAll} href="#">
-          Show All Hours
-        </a>
-      </div>
-    );
-  }
-
-  render() {
-    const { hours } = this.props;
-    if (this.state.showAll) {
-      return this.renderAllHours();
-    } else {
-      return this.renderTodaysHours();
-    }
-  }
-}
+import OpeningHours from "./OpeningHours";
 
 class Details extends React.Component {
   state = {
